045f23f3d4908a2503e136a0699e8f2db9c83ab4
LiardaZZ <sashko_cv@abv.bg>
2018-01-12 00:55:17 +0200

    Refactored json export.

--- DemolitionFalcons-ConsoleVersion/DemolitionFalcons.Models/Player.cs
+++ DemolitionFalcons-ConsoleVersion/DemolitionFalcons.Models/Player.cs
@@ -12,7 +12,7 @@
         }
         public int Id { get; set; }
 
-        public string Type { get; set; }
+        //public string Type { get; set; }
 
         //public byte Picture { get; set; }
 
