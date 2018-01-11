81b14ec2bf93c2442ba9c4124fb20ff47348eba0
zluzunov <zluzunov@gmail.com>
2018-01-12 01:40:22 +0200

    Added games service;

--- DemolitionFalcons-ConsoleVersion/DemolitionFalcons.Service/Controllers/ValuesController.cs
+++ DemolitionFalcons-ConsoleVersion/DemolitionFalcons.Service/Controllers/ValuesController.cs
@@ -1,41 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿
 
 namespace DemolitionFalcons.Service.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using DemolitionFalcons.App.Maps;
+    using DemolitionFalcons.Data;
+    using Front;
+
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class GamesController : Controller
     {
-        // GET api/values 
+        // GET api/game 
         [HttpGet]
-        public IEnumerable<string> Get()
+        public CharacterFront Get()
         {
-            return new string[] { "value1", "value2" };
+            DemolitionFalconsDbContext context = new DemolitionFalconsDbContext();
+
+            var map = new DemoMap("map1");
+
+            var character = context.GameCharacters
+                .Select(c => new CharacterFront(c.Game.Name, map.Name, c.Game.Characters.Count()))
+                .FirstOrDefault();
+
+            return character;
         }
 
-        // GET api/values/5
+        // GET api/game/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST api/game
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/game/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/game/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
---
+++ DemolitionFalcons-ConsoleVersion/DemolitionFalcons.Service/Front/CharacterFront.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemolitionFalcons.Service.Front
+{
+    public class CharacterFront
+    {
+        public CharacterFront(string name, string map, int numberOfOpponents)
+        {
+            this.Name = name;
+            this.Map = map;
+            this.NumberOfOpponents = numberOfOpponents;
+        }
+
+        public string Name { get; set; }
+
+        public string Map { get; set; }
+
+        public int NumberOfOpponents { get; set; }
+    }
+}
