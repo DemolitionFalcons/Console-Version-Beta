70c6702bc007850c969aeffdf3bcaabc2ffaa9e9
Plotso <ilian_1999n@abv.bg>
2018-01-14 21:55:12 +0200

    Created the constructor for AtackACharacter.cs... It takes everything needed to make an atack. It should be called from the ProceedGame method in the GameManager.cs, so that it can have all the parameters that the constructor requires.

--- DemolitionFalcons-ConsoleVersion/DemolitionFalcons.App/Miscellaneous/AttackACharacter.cs
+++ DemolitionFalcons-ConsoleVersion/DemolitionFalcons.App/Miscellaneous/AttackACharacter.cs
@@ -1,5 +1,7 @@
 ﻿namespace DemolitionFalcons.App.Miscellaneous
 {
+    using DemolitionFalcons.App.Maps;
+    using DemolitionFalcons.Data;
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -8,6 +10,21 @@
 
     public class AttackACharacter
     {
-        
+        private DemolitionFalconsDbContext context;
+        private MapSection[][] map;
+        private int roomId;
+        private int characterId;
+        public AttackACharacter(DemolitionFalconsDbContext context,MapSection[][] map, int roomId, int characterId)
+        {
+            this.context = context;
+            this.map = map;
+            this.roomId = roomId;
+            this.characterId = characterId;
+        }
+
+        public void Atack()
+        {
+
+        }
     }
 }
