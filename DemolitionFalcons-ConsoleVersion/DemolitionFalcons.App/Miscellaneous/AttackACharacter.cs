56424d6ea6d00ca90544006672c425301d403485
Plotso <ilian_1999n@abv.bg>
2018-01-18 00:01:00 +0200

    The ting goes skrrrahh Pap, pap, ka-ka-ka Skidiki-pap-pap

--- DemolitionFalcons-ConsoleVersion/DemolitionFalcons.App/Miscellaneous/AttackACharacter.cs
+++ DemolitionFalcons-ConsoleVersion/DemolitionFalcons.App/Miscellaneous/AttackACharacter.cs
@@ -25,13 +25,33 @@
 
         public void Atack()
         {
-            if (!context.GameCharacters.SingleOrDefault(ch => ch.CharacterId == characterId).Spells.Any())
+            if (!context.GameCharacters.SingleOrDefault(ch => ch.CharacterId == characterId ).Spells.Any())
             {
                 throw new ArgumentException("You cannot make an atack due to lack of spells!");
             }
 
+            var spells = context.GameCharacters.SingleOrDefault(ch => ch.CharacterId == characterId && ch.GameId == roomId).Spells.ToList();
+            Console.WriteLine("You can use one of the following spells to atack...");
+            var counter = 1;
+            foreach (var spell in spells)
+            {
+                Console.WriteLine($"{counter}. {spell.Name}");
+                Console.WriteLine($"Description: {spell.Description}");
+                Console.WriteLine($"Damage: { spell.DamageBonus} + your weapon's dmg");
+                Console.WriteLine($"Range: {spell.SpellRange}");
+            }
+            Console.WriteLine($"Please select the number of your desired spell:");
+            var num = int.Parse(Console.ReadLine());
+            while (num <= 0 && num > spells.Count)
+            {
+                Console.WriteLine("Invalid number! Please select a new one:");
+                num = int.Parse(Console.ReadLine());
+            }
+            var chosenSpell = spells[num - 1];
+
             var chNum = context.GameCharacters.FirstOrDefault(c => c.CharacterId == characterId && c.GameId == roomId)
                     .MapSectionNumber;
+            var range = chosenSpell.SpellRange;
 
             var availableCharactersToAtack = new List<Character>();           
             for (int i = 0; i < map.Length; i++)
