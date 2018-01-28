1578f3d0269f1c1ab3bcd13074edff19446630d0
Zlatyo Uzunov <zluzunov@gmail.com>
2018-01-28 10:14:44 +0200

    Changes after defence;

---
+++ DemolitionFalcons-ConsoleVersion/DemolitionFalcons.Service/Controllers/CharacterController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemolitionFalcons.Service.Controllers
+{
+    using App.Commands.DataProcessor;
+    using App.Commands.DataProcessor.Export.Dto;
+    using App.Maps;
+    using Front;
+
+    public class CharacterController : BaseApiController
+    {
+        [HttpGet]
+        public CharacterFront Get()
+        {
+            var map = new DemoMap("map1");
+
+            CharacterFront character = this.dbContext.GameCharacters
+                .Select(c => new CharacterFront
+                {
+                    Map = map.Name,
+                    Name = c.Game.Name,
+                    NumberOfPlayers = c.Game.Characters.Count(),
+                    Players = c.Game.Characters.Select(st => new CharacterDto
+                    {
+                        Type = st.Type,
+                        Nickname = this.dbContext.Players.FirstOrDefault(p => p.Id == st.PlayerId).Username,
+                        Name = st.Character.Name
+                    })
+                })
+                .FirstOrDefault();
+
+            return character;
+
+        }
+    }
+}
--- DemolitionFalcons-ConsoleVersion/DemolitionFalcons.Service/Front/CharacterFront.cs
+++ DemolitionFalcons-ConsoleVersion/DemolitionFalcons.Service/Front/CharacterFront.cs
@@ -5,19 +5,33 @@ using System.Threading.Tasks;
 
 namespace DemolitionFalcons.Service.Front
 {
+    using App.Commands.DataProcessor.Export.Dto;
+
     public class CharacterFront
     {
-        public CharacterFront(string name, string map, int numberOfOpponents)
+        public CharacterFront(string name, string map, int numberOfPlayers)
         {
             this.Name = name;
             this.Map = map;
-            this.NumberOfOpponents = numberOfOpponents;
+            this.NumberOfPlayers = numberOfPlayers;
+        }
+
+        public CharacterFront(string name, string map, int numberOfPlayers, IEnumerable<CharacterDto> players)
+        : this(name, map, numberOfPlayers)
+        {
+            this.Players = players;
+        }
+
+        public CharacterFront()
+        {
         }
 
         public string Name { get; set; }
 
         public string Map { get; set; }
 
-        public int NumberOfOpponents { get; set; }
+        public int NumberOfPlayers { get; set; }
+
+        public IEnumerable<CharacterDto> Players { get; set; }
     }
 }
